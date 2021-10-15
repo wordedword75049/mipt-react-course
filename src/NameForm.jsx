2987d2ca2555e6c3c5f459c06d948866d2296bf3
IvaninNikita <nikita.ivanin@bostongene.com>
2021-10-15 20:42:34 +0300

    Finished with form creating

--- src/NameForm.jsx
+++ src/NameForm.jsx
@@ -6,13 +6,16 @@ export class NameForm extends React.Component {
         super(props);
         this.state = {
             name: '',
-            contact: ''
+            contact: '',
+            exposed: false,
         };
+        this.keys = ['name', 'contact'];
         this.naming_strings = {
-            'name': 'Имя',
-            'contact': 'Контакт для связи'
+            name: 'Имя',
+            contact: 'Контакт для связи'
         }
-
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleClick = this.handleClick.bind(this)
     }
 
     handleInputChange(value, name) {
@@ -21,9 +24,29 @@ export class NameForm extends React.Component {
         });
     }
 
+    handleClick() {
+        if (this.state.exposed === false) {
+            this.setState({
+                exposed: true
+            });
+        }
+    }
+
     render() {
+        let values;
+        if (this.state.exposed) {
+            values = this.keys.map((name) => <div>{this.naming_strings[name]}: {this.state[name]}</div>)
+                }
         return (
+            <form>
             <TextInput naming_strings={this.naming_strings} name='name' onTextChange={this.handleInputChange} value={this.state.name}/>
+            <br/>
+            <TextInput naming_strings={this.naming_strings} name='contact' onTextChange={this.handleInputChange} value={this.state.contact}/>
+            <br/>
+            <label><input type="button" value="Показать введенные данные" onClick={this.handleClick}/></label>
+                <br/>
+                {values}
+            </form>
         );
     }
 }
